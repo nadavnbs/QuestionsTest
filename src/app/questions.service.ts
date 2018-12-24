683bb105dc84011f659867b40d7234a568609efd
nadav ben shimon <nadavnbs@gmail.com>
2018-12-24 17:17:43 +0200

    service update

--- src/app/questions.service.ts
+++ src/app/questions.service.ts
@@ -2,18 +2,18 @@ import { Injectable } from '@angular/core';
 import {Question} from './question';
 // import {Qtype} from './Qtype';
 
-const QUESTION = {personal:[
+const QUESTION = [
   {id:0,headline:'What is your full name',questionType:'explanation'},
   {id:1,headline:'What is your gender',questionType:'single'},
   {id:2,headline:'What is your birthday',questionType:'date'}
-]};
+];
 
 @Injectable()
 export class QuestionsService {
 
   constructor() { }
   getQuestions(){
-    return QUESTION.personal;
+    return QUESTION;
   };
   getQuestion(id:number){
     return this.getQuestions().find((question)=>question.id == id)
@@ -21,6 +21,6 @@ export class QuestionsService {
   addQuestion(question){
     // console.log('question')
     question.id = this.getQuestions().length + 1;
-    QUESTION.personal.push(question)
+    QUESTION.push(question)
   };
 }
